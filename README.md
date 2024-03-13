44e8edd1d9ec588d0a7556664676d7c3b5c510c3
aliola123 <alipoemoses6461@gmail.com>
2024-03-13 18:55:25 +0100

    clean slate

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dray_fingerfoods
+echo "# Dray_fingerfoods" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/aliola123/Dray_fingerfoods.git
+git push -u origin main
---
+++ flask-app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request, redirect, url_for, render_template
+app = Flask(__name__, template_folder='templates')
+
+def is_direct_url():
+    referrer = request.referrer
+    if referrer is None:
+        return True
+    return request.url_root not in referrer
+
+@app.before_request
+def check_direct_url():
+    if is_direct_url() and request.path != '/':
+        return render_template('error.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/shop.html')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/cart.html')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/pdesc.html')
+def pdesc():
+    return render_template('pdesc.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
